28fc0f5da99e366b3ed5ae8e3668babdc7468e52
Luis Alberto Valle <68388768+Luisalberto2020@users.noreply.github.com>
2022-12-13 17:35:02 +0100

    pruebas unitarias

---
+++ PruebasUnitarias/datosPrueba2.py
@@ -0,0 +1,15 @@
+datosCompra = {
+	"id":"123ASSDNN",
+	"estado":"Nuevo",
+	"fecha":"10/02/2022",
+	"tipo":"PC",
+	"descripcion":"Ordenador Lenovo XXLl",
+	"precio":1000.92,
+	"detalle":{
+		"numero":1234,
+		"tienda":"Avda Onu,81",
+		"cliente":"x123pp",
+		"prioridad":"Alta"
+	},
+	"opciones":["sin SO","+32GB RAM","+M.2 512GB"]
+}
--- Python/ejemploPrueba1.py
+++
@@ -1,4 +0,0 @@
-def sumar5(valor):
-    resultado = valor + 5
-    return resultado
-print(sumar5(10))
