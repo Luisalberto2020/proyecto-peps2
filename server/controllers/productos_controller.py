9a0cbbeec431e67ac4df06d11432166a04d91313
Luis Alberto Valle <68388768+Luisalberto2020@users.noreply.github.com>
2022-12-14 17:00:22 +0100

    commit ayer

--- server/controllers/productos_controller.py
+++ server/controllers/productos_controller.py
@@ -84,25 +84,92 @@ def delete_producto(id):
     headers = request.headers
     if headers['token'] and Jwtutils().is_valido(headers['token']) and Jwtutils().is_admin(headers['token']):
         try:
-            data = json.loads(request.data)
             producto_repository = ProductoRepository()
-            producto_repository.delete_producto(id)
+            producto_repository.delete_producto(int(id))
             code = 200
             response = {
                 'code': code,
-                'message': 'Eliminado correctamente',
+                'message': 'Eliminado correctamente ',
             }
         except Exception as e:
-            code = 401
+            code = 400
             response = {
                 'code': code,
-                'message': f'Error al eliminar producto {e}',
+                'message': f'Error al eliminar producto {id}',
             }
     else:
         code = 401
         response = {
             'code': code,
             'message': 'Token invalido',
+        }
+
+    return json.dumps(response), code
+
+@productos_bluebrint.route('/updateproducto', methods=['PUT'])
+@cross_origin()
+def update_producto():
+    code = 401
+    response = {
+            'code': code,
+            'message': 'Error'
+        }
+    headers = request.headers
+    if headers['Content-Type'] != 'application/json':
+        code = 401
+        response = {
+            'code': code,
+            'message': 'Content-Type debe ser application/json'
+        }
+    else:
+        if headers['token']:
+            print(headers['token'])
+            if Jwtutils().is_valido(headers['token']):
+                print('token valido')
+                if Jwtutils().is_admin(headers['token']):
+                    print('es admin')
+                
+                    try:
+                        data = json.loads(request.data)
+                        nombre = data['nombre']
+                        precio = data['precio']
+                        producto_repository = ProductoRepository()
+                        producto_repository.update_producto(data['id'],Markup(nombre), Markup(precio), data['url'])
+                        code = 200
+                        response = {
+                        'code': code,
+                        'message': 'actualizado correctamente'
+                        }
+                    except Exception as e:
+                        code = 401
+                        response = {
+                            'code': code,
+                            'message': f'Error al actualizar producto {e}',
+                        }
+            else:
+                code = 401
+                response = {
+                    'code': code,
+                    'message': 'Token inválido',
+                }
+
+    return json.dumps(response), code
+
+@productos_bluebrint.route('/getproducto/<id>', methods=['GET'])
+@cross_origin()
+def get_producto(id):
+    try:
+        producto_repository = ProductoRepository()
+        producto = producto_repository.get_producto(id)
+        response = producto.to_dict()
+        code = 200
+
+
+    except Exception as e:
+        code = 401
+        response = {
+            'code': code,
+            'message': f'Error al obtener producto {e}',
         }
 
     return json.dumps(response), code
